9b1f8e6d62ab81d8c2c775909625204d98476de7
sonluis <sonhong121997@gmail.com>
2019-05-22 20:55:40 +0700

    ifelse

---
+++ ifelse.php
@@ -0,0 +1,13 @@
+<?php
+$tuoi = 30;
+if ($tuoi >25){
+    echo "bạn la người trưởng thanh";
+}
+else
+    echo 'bạn là người già or trẻ em';
+/**
+ * Created by PhpStorm.
+ * User: sonpt
+ * Date: 5/22/2019
+ * Time: 8:53 PM
+ */
